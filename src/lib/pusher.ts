153aba73a30795ba3a10cd268a6263e20be1a58c
ante.mikolcevic <ante.mikolcevic@posta.hr>
2026-01-25 22:37:21 +0100

    fix: provide default cluster for pusher to prevent build error

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -2,16 +2,16 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
-    appId: process.env.PUSHER_APP_ID!,
-    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-    secret: process.env.PUSHER_APP_SECRET!,
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    appId: process.env.PUSHER_APP_ID || "",
+    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY || "",
+    secret: process.env.PUSHER_APP_SECRET || "",
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "eu",
     useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-    process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    process.env.NEXT_PUBLIC_PUSHER_APP_KEY || "",
     {
-        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "eu",
     }
 );
